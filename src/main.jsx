69174a7f72c21e5ee7a845f744a37287c26011c1
onnez7 <ronifaria2018@gmail.com>
2025-05-06 15:01:00 -0300

    Corrigir build na Vercel removendo vite.config.ts do src e ajustando vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { App }  from './App';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <BrowserRouter basename="/subdiretorio">
+  <App />
+</BrowserRouter>
 );
